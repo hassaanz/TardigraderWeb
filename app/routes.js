a617b896fff6ca56bdeec1e508047d25d06ac9d4
Hassaan Zaidi <hassaanz@gmail.com>
2017-11-06 16:21:49 +0100

    Initial website setup

---
+++ app/routes.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var router = express.Router()
+
+// Route index page
+router.get('/', function (req, res) {
+  res.render('index')
+})
+
+router.get('/web', function (req, res) {
+  res.render('websiteHome')
+})
+
+// add your routes here
+
+module.exports = router
